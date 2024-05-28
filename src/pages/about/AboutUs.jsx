00ae574dd6d99d15bb879b40af98ae5961b860bc
fazlibeqir <fazlibeqir@gmail.com>
2024-05-28 16:48:10 +0200

    Vercel & AboutUs FIX

--- src/pages/about/AboutUs.jsx
+++ src/pages/about/AboutUs.jsx
@@ -1,15 +1,32 @@
-import { Container, ImageList, ImageListItem, ImageListItemBar, Typography } from "@mui/material";
+import { useState, useEffect } from 'react';
+import { Container, ImageList, ImageListItem, ImageListItemBar } from "@mui/material";
 import { team } from "../../assets/team.json";
 
 export const AboutUs = () => {
-    console.log(team);
+    const [images, setImages] = useState({});
+
+    useEffect(() => {
+        const importImages = async () => {
+            const importedImages = {};
+            for (const member of team) {
+                const { default: image } = await import(`../../assets/members/${member.image}.jpg`);
+                importedImages[member.image] = image;
+            }
+            setImages(importedImages);
+        };
+
+
+
+        importImages().then(() => console.log("Images loaded"));
+    }, []);
+
     return (
         <Container sx={{ height: '100vh', display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', overflow: 'hidden'}}>
             <ImageList sx={{ width: '80%', height: '80%', overflow: 'hidden' }} cols={5}>
                 {team.map((member) => (
                     <ImageListItem key={member.id} cols={1}>
                         <img
-                            src={`/src/assets/members/${member.image}.jpg`}
+                            src={images[member.image]}
                             alt={member.name}
                             loading="lazy"
                         />
